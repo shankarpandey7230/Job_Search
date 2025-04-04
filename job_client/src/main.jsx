aaa880c137e57466337a2ede5a3b637f448a9f22
shankarpandey7230 <shankarpandey7230@gmail.com>
2025-04-04 23:58:16 +1100

    persisting our store

--- job_client/src/main.jsx
+++ job_client/src/main.jsx
@@ -1,20 +1,24 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import {Provider} from 'react-redux'
-import store from './redux/store'
-import './index.css';
-import { Toaster } from '@/components/ui/sonner';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import "./index.css";
+import { Toaster } from "@/components/ui/sonner";
 
-import App from './App.jsx';
-
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+const persistor = persistStore(store);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-<Provider store={store}>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
+    <Toaster />
   </StrictMode>
 );

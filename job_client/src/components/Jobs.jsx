6c769162fd5d3312b5f55645b75f02307c52dbbf
shankarpandey7230 <shankarpandey7230@gmail.com>
2025-04-26 04:55:24 +1000

    adding framer motion

--- job_client/src/components/Jobs.jsx
+++ job_client/src/components/Jobs.jsx
@@ -3,7 +3,7 @@ import Navbar from "./shared/Navbar";
 import FilterCard from "./FilterCard";
 import Job from "./Job";
 import { useSelector } from "react-redux";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 // const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
 
@@ -40,7 +40,15 @@ const Jobs = () => {
             <div className="flex-1 h-[88vh] overflow-y-auto pb-5">
               <div className="grid grid-cols-3 gap-4">
                 {filterJobs.map((job) => (
-                  <Job job={job} key={job._id} />
+                  <motion.div
+                    initial={{ opacity: 0, x: 100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    exit={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 0.3 }}
+                    key={job?._id}
+                  >
+                    <Job job={job} />
+                  </motion.div>
                 ))}
               </div>
             </div>

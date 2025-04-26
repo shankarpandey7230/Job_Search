2bf212c2f7c8e59ee9a3f0224d1beef8b12660ac
shankarpandey7230 <shankarpandey7230@gmail.com>
2025-04-27 04:01:28 +1000

    changes

--- job_client/src/utils/apiCall.jsx
+++ job_client/src/utils/apiCall.jsx
@@ -1,8 +1,8 @@
 export const USER_API_END_POINT =
-  "https://job-search-kyjr.onrender.com/api/v1/user";
+  "https://job-search-qt3j.onrender.com/api/v1/user";
 export const JOB_API_END_POINT =
-  "https://job-search-kyjr.onrender.com/api/v1/jobs";
+  "https://job-search-qt3j.onrender.com//api/v1/jobs";
 export const APPLICATION_API_END_POINT =
-  "https://job-search-kyjr.onrender.com/api/v1/application";
+  "https://job-search-qt3j.onrender.com//api/v1/application";
 export const COMPANY_API_END_POINT =
-  "https://job-search-kyjr.onrender.com/api/v1/company";
+  "https://job-search-qt3j.onrender.com/api/v1/company";

d057a6131e4e03580b1c0a573fd86369ff5cfc48
shankarpandey7230 <shankarpandey7230@gmail.com>
2025-04-27 02:37:15 +1000

    changes

--- job_client/src/components/admin/CompanySetup.jsx
+++ job_client/src/components/admin/CompanySetup.jsx
@@ -10,9 +10,11 @@ import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "sonner";
 import { useSelector } from "react-redux";
 import useGetCompanyById from "@/hooks/useGetCompanyById";
+import useGetAllCompanies from "../../hooks/useGetAllCompanies";
 
 const CompanySetup = () => {
   const params = useParams();
+
   useGetCompanyById(params.id);
   const [input, setInput] = useState({
     name: "",

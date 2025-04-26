eeba34d30633f0ae620204cfca75f7a0ed8e97d6
shankarpandey7230 <shankarpandey7230@gmail.com>
2025-04-27 02:05:18 +1000

    error

--- job_client/src/components/admin/AdminJobsTable.jsx
+++ job_client/src/components/admin/AdminJobsTable.jsx
@@ -21,6 +21,7 @@ const AdminJobsTable = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
+    console.log("called");
     const filteredJobs = allAdminJobs.filter((job) => {
       if (!searchJobByText) {
         return true;
@@ -57,9 +58,7 @@ const AdminJobsTable = () => {
                   </PopoverTrigger>
                   <PopoverContent className="w-32">
                     <div
-                      onClick={() =>
-                        navigate(`/admin/companies/${job.company._id}`)
-                      }
+                      onClick={() => navigate(`/admin/companies/${job._id}`)}
                       className="flex items-center gap-2 w-fit cursor-pointer"
                     >
                       <Edit2 className="w-4" />

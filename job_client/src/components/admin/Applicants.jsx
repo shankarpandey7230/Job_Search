3e6b1886587af977b91c2c37d912028584c85320
shankarpandey7230 <shankarpandey7230@gmail.com>
2025-04-27 01:38:31 +1000

    fixed

--- job_client/src/components/admin/Applicants.jsx
+++ job_client/src/components/admin/Applicants.jsx
@@ -7,21 +7,26 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { setAllApplicants } from "../../redux/applicationSlice";
 import NavBar from "../shared/NavBar";
-
+import { toast } from "sonner";
 const Applicants = () => {
   const params = useParams();
+  const id = params.id;
   const dispatch = useDispatch();
   const { applicants } = useSelector((store) => store.application);
+
   useEffect(() => {
     const fetchAllApplicants = async () => {
       try {
+        axios.defaults.withCredentials = true;
         const res = await axios.get(
-          `${APPLICATION_API_END_POINT}/${params.id}/applicants`,
-          { withCredentials: true }
+          `${APPLICATION_API_END_POINT}/${id}/applicants`
         );
-        dispatch(setAllApplicants(res.data.job));
+        if (res.data.success) {
+          dispatch(setAllApplicants(res.data.job));
+        }
       } catch (error) {
         console.log(error);
+        toast.error(error.response.data.message);
       }
     };
     fetchAllApplicants();
@@ -30,8 +35,8 @@ const Applicants = () => {
     <div>
       <NavBar />
       <div className="max-w-7xl mx-auto">
-        <h1 className="font-bold text-xl my-5">
-          Applicants {applicants?.applications?.length}
+        <h1 className="font-medium text-xl my-5">
+          Applicants ({applicants?.applications.length})
         </h1>
         <ApplicantsTable />
       </div>
